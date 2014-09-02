07bf477783726aed869246ae0b3031a729deea08
Eugene Kadish <eugenekadish@gmail.com>
2014-09-02 13:53:52 -0400

    Add a test and styling to the test runner.

--- tests/tests.js
+++ tests/tests.js
@@ -1,18 +1,31 @@
-test(' Reading the object file.', function(){
-   deepEqual(
-      objectImport('../example/data/cube.obj')['vertices'], 
-      [0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1,
-       0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1,
-       0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1,
-       0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1,
-       0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 
-       0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-       1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1,
-       1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1,
-       0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 
-       0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1,
-       0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
-       0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-       ' The cube.obj file was processed correctly.'
+/**
+ * Check that formatting object files of some basic shapes is done correctly.
+ */
+QUnit.test(' Reading the object file.', function(assert){
+  
+  assert.deepEqual(
+    objectImport('../example/data/tetrahedron.obj')['vertices'],
+      [1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 
+       1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 
+       1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1,
+       2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1], 
+       ' Checked loading in a tetrahedron.'
+	);
+
+  assert.deepEqual(
+    objectImport('../example/data/cube.obj')['vertices'], 
+        [0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1,
+         0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1,
+         0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1,
+         0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1,
+         0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 
+         0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1,
+         1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+         1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1,
+         0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 
+         0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1,
+         0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+         0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+         ' Checked loading in a cube.'
       );
 });
