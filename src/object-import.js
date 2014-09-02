990f8920c1fbfba166995eb41cea05c86074e424
Eugene Kadish <eugenekadish@gmail.com>
2014-09-02 12:41:39 -0400

    Group styles into a separate file.

--- src/object-import.js
+++ src/object-import.js
@@ -1,86 +1,93 @@
 /**
-  * Opens an object file, reads the data, and formats it so that it can be passed to
-  * buffers for rendering.
-  *
-  * @param  {String} path The relative path of the obj file.
-  * @return {Array}       An array of arrays, each of which is either the vertices, textures, 
-  *                       or normals corresponding to the triangles making the object.
-  */
+ * Opens an object file, reads the data, and formats it so that it can be passed to
+ * buffers for rendering.
+ *
+ * @param  {String} path The relative path of the obj file.
+ * @return {Array}       An array of arrays, each of which is either the vertices, textures, 
+ *                       or normals corresponding to the triangles making the object.
+ */
 function objectImport(path){
 
-   var objectReader = new XMLHttpRequest();
+  var objectReader = new XMLHttpRequest();
   
-   var file, line;
+  var file, line;
 
-   var vertices = [];
-   var textures = [];
-   var normals  = [];
-   var indices  = [];
+  var vertices = [];
+  var textures = [];
+  var normals  = [];
+  var indices  = [];
 
-   var bufferData = [];
+  var bufferData = [];
 
-   bufferData['vertices'] = [];
-   bufferData['textures'] = [];
-   bufferData['normals']  = [];
+  bufferData['vertices'] = [];
+  bufferData['textures'] = [];
+  bufferData['normals']  = [];
 
-   objectReader.open('GET', path, false);
+  objectReader.open('GET', path, false);
 
-   // Load the object file synchronously.
-   objectReader.onload = function(){
+  // Load the object file synchronously.
+  objectReader.onload = function(){
     
-      if(objectReader.readyState == 4){
-         if(objectReader.status == 200){
+    if(objectReader.readyState == 4){
+      if(objectReader.status == 200){
         
-            // Empty arrays are filtered out. They are returned
-            // for every blank line in the file.
-            file = objectReader
-                     .responseText
-                     .split('\n')
-                     .filter(function(item){
-                        return item != [];
-                     });
-         } else {
-            console.log(' Their was an error loading the object file. \n');
-         }
+        // Empty arrays are filtered out. They are returned
+        // for every blank line in the file.
+        file = objectReader
+                   .responseText
+                   .split('\n')
+                   .filter(function(item){
+                     return item != [];
+                   });
+      } else {
+          console.log(' Their was an error loading the object file. \n');
       }
-   };
+    }
+  };
 
-   objectReader.send(null);
+  objectReader.send(null);
 
-   file.forEach(function(line){
+  file.forEach(function(line){
     
-      line = line.split(' ')
-                 .filter(function(item){
-                    return item != ''; 
-                 });
+    line = line.split(' ')
+               .filter(function(item){
+                 return item != ''; 
+               });
     
-      if(line[0] === 'v'){
-         line[1] = parseFloat(line[1]);
-         line[2] = parseFloat(line[2]);
-         line[3] = parseFloat(line[3]);
-         vertices.push(line.slice(1).concat([1.0]));
-      } else if(line[0] === 'vt'){
-         line[1] = parseFloat(line[1]);
-         line[2] = parseFloat(line[2]);
-         textures.push(line.slice(1));
-      } else if(line[0] === 'vn'){ 
-         line[1] = parseFloat(line[1]);
-         line[2] = parseFloat(line[2]);
-         line[3] = parseFloat(line[3]);
-         normals.push(line.slice(1));
-      } else if(line[0] === 'f'){
+    if(line[0] === 'v'){
+      line[1] = parseFloat(line[1]);
+      line[2] = parseFloat(line[2]);
+      line[3] = parseFloat(line[3]);
+      vertices.push(line.slice(1).concat([1.0]));
+    } else if(line[0] === 'vt'){
+      line[1] = parseFloat(line[1]);
+      line[2] = parseFloat(line[2]);
+      textures.push(line.slice(1));
+    } else if(line[0] === 'vn'){ 
+      line[1] = parseFloat(line[1]);
+      line[2] = parseFloat(line[2]);
+      line[3] = parseFloat(line[3]);
+      normals.push(line.slice(1));
+    } else if(line[0] === 'f'){
 
-         for(var i = 1; i < line.length; i++){
-            indices = indices.concat(line[i].split('/'));
-         }
+      for(var i = 1; i < line.length; i++){
+        indices = indices.concat(line[i].split('/'));
       }
-   });
+    }
+  }); 
 
-   for(var k = 0; k < indices.length; k = k + 3){
-      bufferData['vertices'] = bufferData['vertices'].concat(vertices[indices[k] - 1]);
+  for(var k = 0; k < indices.length; k = k + 3){
+    
+    bufferData['vertices'] = bufferData['vertices'].concat(vertices[indices[k] - 1]);
+    
+    if(textures.length > 0){
       bufferData['textures'] = bufferData['textures'].concat(textures[indices[k + 1] - 1]);
-      bufferData['normals']  = bufferData['normals'].concat(normals[indices[k + 2] - 1]); 
-   }
+    }
 
-   return bufferData;
+    if(normals.length > 0){
+      bufferData['normals']  = bufferData['normals'].concat(normals[indices[k + 2] - 1]); 
+    }
+  }
+
+  return bufferData;
 }
